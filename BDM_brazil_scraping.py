0b2a45180c8287584b2aa4e1f4caf6dc0e451edf
NiccoloMorabito <morabito.niccolo@gmail.com>
2022-03-29 15:32:18 +0200

    Fixes and updates in the scripts for Brazil and Chile

--- BDM_brazil_scraping.py
+++ BDM_brazil_scraping.py
@@ -1,3 +1,4 @@
+from operator import contains
 from bs4 import BeautifulSoup
 import requests
 from os import makedirs
@@ -10,13 +11,19 @@ BRAZIL_FOLDER = join(dirname(abspath(__file__)), "data/brazil")
 BRAZIL_URL = "https://www.gov.br/produtividade-e-comercio-exterior/pt-br/assuntos/comercio-exterior/estatisticas/base-de-dados-bruta"
 COMMON_PART_IN_LINK = "https://balanca.economia.gov.br/balanca/bd/"
 
-if __name__ == '__main__':
-    req = requests.get(BRAZIL_URL).text
+def get_all_links_in(url):
+    req = requests.get(url).text
     soup = BeautifulSoup(req, features='lxml')
 
-    for link in soup.findAll("a"):
+    return soup.findAll("a")
+
+if __name__ == '__main__':
+    #TODO run this every x days
+
+    #TODO check what data is already downloaded not to download it again
+    for link in get_all_links_in(BRAZIL_URL):
         current_link = link.get("href")
-        if current_link and current_link.endswith('csv'):
+        if current_link and (current_link.endswith('.csv') or current_link.endswith('.xlsx')):
             print(current_link)
             print(f"\t{current_link.replace(COMMON_PART_IN_LINK, '')}")
 
--- BDM_chile_scraping.py
+++ BDM_chile_scraping.py
@@ -1,51 +1,95 @@
 from bs4 import BeautifulSoup
 import requests
-from os import makedirs
-from os.path import abspath, dirname, join
-import patoolib
+import os
+from rarfile import RarFile
+import re
 
 import urllib3
 urllib3.disable_warnings()
 
-CHILE_FOLDER = join(dirname(abspath(__file__)), "data/chile")
+CHILE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/chile")
 CHILE_URL = "https://datos.gob.cl/organization/servicio_nacional_de_aduanas?page={}"
+HEADER = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+
+def get_new_datasets(existing_folders):
+    datasets = get_datasets_from_all_pages()
+    print(f"Folders that are going to be ignored because already downloaded:")
+    print({dataset for dataset in datasets if dataset.split('/')[2] in existing_folders})
+    return {dataset for dataset in datasets if dataset.split("/")[2] not in existing_folders}
+
+def get_datasets_from_all_pages():
+    # collect the different datasets from the main site by iterating on the different pages
+    page=1
+    datasets = set()
+    newpage_datasets = get_datasets_in(page)
+    while newpage_datasets:
+        datasets |= newpage_datasets
+        page += 1
+        newpage_datasets = get_datasets_in(page)
+    
+    return datasets
+
+def get_datasets_in(page):
+    url = CHILE_URL.format(page)
+    req = requests.get(url, headers=HEADER).text
+    soup = BeautifulSoup(req, features='lxml')
+    return {link.get("href") for link in soup.findAll("a") if link.get("href").startswith("/dataset/")}
+
+def extract_rars_in(folderpath):
+    '''
+    Extracts all rar files in the folder and deletes the rar files afterwards
+    '''
+    for file in os.listdir(folderpath):
+        #TODO check if this regex works
+        if re.match("\.part[0-9][2-9]\.rar", file):
+            continue
+        if file.endswith(".rar"):
+            filepath = os.path.join(folder, file)
+            try:
+                rf = RarFile(filepath)
+                rf.extractall(folderpath)
+            except:
+                print(f"Error extracting file: {filepath}") #TODO
+                continue
+    
+    for file in os.listdir(folderpath):
+        if file.endswith(".rar"):
+            filepath = os.path.join(folder, file)
+            os.remove(filepath)
 
 if __name__ == '__main__':
-    #TODO change names of variables to make code more understandable
-    HDR = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
-
-    # collect the different datasets from the main site by iterasting on the different pages
-    datasets = set()
-    page=1
-    while True:
-        req = requests.get(CHILE_URL.format(page), headers=HDR).text
-        soup = BeautifulSoup(req, features='lxml')
-        new_datasets = {link.get("href") for link in soup.findAll("a") if link.get("href").startswith("/dataset/")}
-        if not new_datasets:
-            break
-        datasets |= new_datasets
-        page += 1
+    #TODO run this every x days
     
-    for dataset in datasets:
+    #TODO this code to check what data is already downloaded won't work in the temporary landing setting
+    if os.path.isdir(CHILE_FOLDER):
+        existing_folders = set(os.listdir(CHILE_FOLDER))
+    else:
+        existing_folders = set()
+    new_datasets = get_new_datasets(existing_folders)
+
+    for dataset in new_datasets:
         current_link = "https://datos.gob.cl" + dataset
         print(current_link)
 
-        url_content = requests.get(current_link, verify=False, headers=HDR).content
+        url_content = requests.get(current_link, verify=False, headers=HEADER).content
         subsoup = BeautifulSoup(url_content, features='lxml')
         for sublink in subsoup.findAll("a"):
             download_link = sublink.get("href")
             if download_link and (download_link.endswith('xlsx') or download_link.endswith('rar')):
-                file_content = requests.get(download_link, verify=False, headers=HDR).content
+                file_content = requests.get(download_link, verify=False, headers=HEADER).content
                 # filepath is the CHILE_FOLDER + folder specifying import/export and year + file name
-                filepath = join(CHILE_FOLDER, current_link.split("/")[-1], download_link.split("/")[-1])
+                filepath = os.path.join(CHILE_FOLDER, current_link.split("/")[-1], download_link.split("/")[-1])
                 print("\t" + filepath)
                 
-                makedirs(dirname(filepath), exist_ok=True)
+                os.makedirs(os.path.dirname(filepath), exist_ok=True)
                 with open(filepath, 'wb') as file:
                     file.write(file_content)
-                '''
-                RAR FILES NEED TO BE EXTRACTED
-                if filepath.endswith(".rar"):
-                    patoolib.extract_archive(filepath, outdir=filepath.split("/")[:-1])
-                '''
+    
+    # extract RAR files
+    print("\n\nExtracting...")
+    for file in os.listdir(CHILE_FOLDER):
+        folder = os.path.join(CHILE_FOLDER, file)
+        if os.path.isdir(folder):
+            print("Working with: " + folder)
+            extract_rars_in(folder)
 
