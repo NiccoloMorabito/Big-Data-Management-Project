b05de8cebb14e54e095a2820c6efedc0df16b628
NiccoloMorabito <morabito.niccolo@gmail.com>
2022-03-31 23:47:27 +0200

    Update in Brazil loader script to download the data into HDFS

--- collectors/brazil.py
+++ collectors/brazil.py
@@ -2,12 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 from os import makedirs
 from os.path import dirname, join
+from hdfs import InsecureClient
 from json_utils import load_seen_files, save_seen_files
+
+from signal import signal, SIGPIPE, SIG_DFL 
+#Ignore SIG_PIPE and don't throw exceptions on it
+signal(SIGPIPE,SIG_DFL) 
 
 import urllib3
 urllib3.disable_warnings()
 
-BRAZIL_FOLDER = "../data/brazil"
+BRAZIL_FOLDER = "/data/brazil"
 BRAZIL_URL = "https://www.gov.br/produtividade-e-comercio-exterior/pt-br/assuntos/comercio-exterior/estatisticas/base-de-dados-bruta"
 COMMON_PART_IN_LINK = "https://balanca.economia.gov.br/balanca/bd/"
 BRAZIL_SEENFILES_PATH = "brazil.json"
@@ -21,6 +26,8 @@ def get_all_links_in(url):
 if __name__ == '__main__':
     #TODO run this every x days
 
+    client = InsecureClient('http://127.0.0.1:9870', user='bdm')
+
     seen_files = load_seen_files(BRAZIL_SEENFILES_PATH)
 
     for link in get_all_links_in(BRAZIL_URL):
@@ -31,10 +38,11 @@ if __name__ == '__main__':
                 continue
 
             filepath = join(BRAZIL_FOLDER, dataset_name)
-            makedirs(dirname(filepath), exist_ok=True)
-            url_content = requests.get(current_link, verify=False).content
-            with open(filepath, 'wb') as csv_file:
-                csv_file.write(url_content)
+
+            url_content = requests.get(current_link, verify=False).content.decode('utf-8')
+            
+            with client.write(filepath, encoding='utf-8') as writer:
+                writer.write(url_content)
 
             print(f"{current_link}\t({dataset_name})")
             
